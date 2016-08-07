ef73f220aaf4b85cbb6dce7346b158b99a181da7
Plain <mxh403@163.com>
2016-08-07 18:19:02 +0800

    cnblogs1.1 js

---
+++ cnblogs/1.1html and css/1.1js.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Plain on 2016/8/7.
+ */
+<!--PageBeginHtml Block Begin-->
+<script type="text/javascript">
+window.console&&console.log("去除google广告！避免浏览器状态栏一直显示加载");
+var canShowAdsense=function(){return !!0};
+//setTimeout(function(){
+//$("iframe[id^=google],iframe[src*=google]").remove();},2000);
+// 首页html代码
+</script>
+<div class="page-header">
+
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+<link rel="stylesheet" href="http://files.cnblogs.com/files/mxh1099/bootstrap.min.css">
+<script src="http://files.cnblogs.com/files/lianmin/jquery1.11.1.js"></script>
+<script src="http://files.cnblogs.com/files/lianmin/bootstrap.min.js"></script>
+<script src="http://files.cnblogs.com/files/lianmin/bootbox.min.js"></script>
+<link rel="stylesheet" href="http://files.cnblogs.com/files/lianmin/slider.min.css"/>
+
+<div style="margin-bottom: 20px; height:118px;"><!--box-shadow: 0 2px 16px #ddd;background-color:#fff;-->
+<div id="imgBar" style="margin:0 auto;width:1280px;overflow:hidden; height:360px;">
+</div>
+</div>
+<script src="http://files.cnblogs.com/files/lianmin/slider1.2.min.js"></script>
+<script type="text/javascript">
+    $("#imgBar").slider({
+        imgs: [
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_1433497098223.jpg",
+            "",
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_1436251155798.jpg",
+            "",
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_%e5%9b%9b%e6%9c%88%e6%98%af%e4%bd%a0%e7%9a%84%e8%b0%8e%e8%a8%80.jpg",
+            "",
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_chunyinweilai.jpg",
+            "",
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_1437459761612.jpg",
+            "",
+            "http://images.cnblogs.com/cnblogs_com/mxh1099/787157/o_1436754741343.jpg",
+            ""],
+        scale: 128 / 36,
+        border: true,
+        delay: 2200,
+        x: 14,
+        y: 4
+    });
+</script>
+<!--PageBeginHtml Block End-->
+
+//*<script>
+//$(document).ready($("#div_digg").html("<span class=\"diggnum\" id=\"digg_countt\">99</span>"))
+//$(document).ready($(".postDesc").html('posted @ <span id="post-date">2016-04-13 16:15</span> <a //href="http://www.cnblogs.com/mxh1099/">拿铁</a> 阅读(<span id="ppost_view_count">1999</span>) 评论(<span //id="post_comment_count">0</span>)  <a href="http://i.cnblogs.com/EditPosts.aspx?postid=5386529" rel="nofollow">编辑//</a> <a href="#" onclick="AddToWz(5386529);return false;">收藏</a>'))
+//$(document).ready(console.log("哈哈，怎么还是不行呢？"+ $("#post_view_count").text()))
+//$(document).ready(console.log("哈哈，div-digg"+ $("#div_digg").text(9000)))
+//$(document).ready($(".diggit").html('<span class="diggnum" id="">99</span>'))
+//</script>*/
